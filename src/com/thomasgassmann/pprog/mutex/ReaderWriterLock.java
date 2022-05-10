03e24a2201e6f3fe3f82f12c69a35010b9a2bf59
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-05-10 19:03:13 +0200

    make lock "fairer"
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- src/com/thomasgassmann/pprog/mutex/ReaderWriterLock.java
+++ src/com/thomasgassmann/pprog/mutex/ReaderWriterLock.java
@@ -4,15 +4,23 @@ package com.thomasgassmann.pprog.mutex;
 // TODO: exercise Exercise: come up with a
 // better performing version
 // using condition variables!
+// Introduce an upper bound of k readers!
 public class ReaderWriterLock {
-    int writers = 0;
-    int readers = 0;
-    int writersWaiting = 0;
+    int writers = 0; // writers in CS
+    int readers = 0; // readers in CS
+    int writersWaiting = 0; // writers trying to enter CS
+    int readersWaiting = 0; // readers trying to enter CS
+    int writersWait = 0; // number of readers the writers have to wait for
 
     synchronized void acquire_read() {
-        while (writers > 0 || writersWaiting > 0)
+        readersWaiting++;
+        // Writers are waiting and the readers don't
+        // have priority any more.
+        while (writers > 0 || (writersWaiting > 0 && writersWait <= 0))
             try { wait(); }
             catch (InterruptedException e) {}
+        readersWaiting--;
+        writersWait--;
         readers++;
     }
 
@@ -23,7 +31,9 @@ public class ReaderWriterLock {
 
     synchronized void acquire_write() {
         writersWaiting++;
-        while (writers > 0 || readers > 0)
+        // Writers have to wait until the waiting readers
+        // have finished
+        while (writers > 0 || readers > 0 || writersWait > 0)
             try { wait(); }
             catch (InterruptedException e) {}
         writersWaiting--;
@@ -32,6 +42,9 @@ public class ReaderWriterLock {
 
     synchronized void release_write() {
         writers--;
+        // When a writer finishes, the number of
+        // currently waiting readers may pass.
+        writersWait = readersWaiting;
         notifyAll();
     }
 }
